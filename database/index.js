bb574e1d2728c0a95fc3d94700f30df2c5e9b467
Vincent Castelli <vincentsalvatorecastelli@gmail.com>
2018-05-25 11:39:41 -0700

    Defined mongoDb schema; fixed bug within search component.

--- database/index.js
+++ database/index.js
@@ -2,7 +2,13 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+  id: Number,
+  fullname: String,
+  owner: { id: Number, login: String, url: String, repos_url: String },
+  private: Boolean,
+  description: String,
+  created_at: Date,
+  updated_at: Date
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
